51c02b382a9752dec06bc9985eb1208bd3c55c36
khannaanurag1 <khannaanurag1@gmail.com>
2025-01-15 20:59:40 +0530

    first commut

---
+++ src/main/java/org/example/userauthenticationservice_dec2025/UserAuthenticationServiceDec2025Application.java
@@ -0,0 +1,13 @@
+package org.example.userauthenticationservice_dec2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationServiceDec2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthenticationServiceDec2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/userauthenticationservice_dec2025/UserAuthenticationServiceDec2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.userauthenticationservice_dec2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthenticationServiceDec2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
