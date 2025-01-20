cd7147d4970d370a7f266bb2cac9da84d09314eb
khannaanurag1 <khannaanurag1@gmail.com>
2025-01-20 23:37:09 +0530

    Spring OAuth

---
+++ src/main/java/org/example/userauthenticationservice_dec2025/security/CustomUserDetails.java
@@ -0,0 +1,33 @@
+package org.example.userauthenticationservice_dec2025.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.example.userauthenticationservice_dec2025.models.User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+
+    private User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
